6a4fe2b28c168c2c8d1eab7861ace9a021a7882d
Mike Nicholaides <mike.nicholaides@gmail.com>
2014-08-01 16:15:18 -0400

    Clean up pull request example hash

--- app/models/pull_request_example.rb
+++ app/models/pull_request_example.rb
@@ -5,44 +5,22 @@ class PullRequestExample
     {
       'description' => 'This is my pull request',
       'links' => {
-        'decline' => {
-          'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/decline'
-        },
-        'commits' => {
-          'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/commits'
-        },
-        'self' => {
-          'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211'
-        },
-        'comments' => {
-          'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/comments'
-        },
-        'merge' => {
-          'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/merge'
-        },
-        'html' => {
-          'href' => 'https://bitbucket.org/jenkins-bitbucket/jenkins-bitbucket/pull-request/211'
-        },
-        'activity' => {
-          'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/activity'
-        },
-        'diff' => {
-          'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/diff'
-        },
-        'approve' => {
-          'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/approve'
-        }
+        'decline'  => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/decline' },
+        'commits'  => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/commits' },
+        'self'     => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211' },
+        'comments' => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/comments' },
+        'merge'    => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/merge' },
+        'html'     => { 'href' => 'https://bitbucket.org/jenkins-bitbucket/jenkins-bitbucket/pull-request/211' },
+        'activity' => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/activity' },
+        'diff'     => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/diff' },
+        'approve'  => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/pullrequests/211/approve' }
       },
       'author' => {
         'username' => 'jenkins-bitbucket',
         'display_name' => 'jenkins-bitbucket jenkins-bitbucket',
         'links' => {
-          'self' => {
-            'href' => 'https://api.bitbucket.org/2.0/users/jenkins-bitbucket'
-          },
-          'avatar' => {
-            'href' => 'https://secure.gravatar.com/avatar/d1de01807f6b367718a207845dfc8a64?d=https%3A%2F%2Fd3oaxc4q5k2d6q.cloudfront.net%2Fm%2Fdec446fd616d%2Fimg%2Fdefault_avatar%2F32%2Fuser_blue.png&s=32'
-          }
+          'self'   => { 'href' => 'https://api.bitbucket.org/2.0/users/jenkins-bitbucket' },
+          'avatar' => { 'href' => 'https://secure.gravatar.com/avatar/d1de01807f6b367718a207845dfc8a64?d=https%3A%2F%2Fd3oaxc4q5k2d6q.cloudfront.net%2Fm%2Fdec446fd616d%2Fimg%2Fdefault_avatar%2F32%2Fuser_blue.png&s=32' }
         }
       },
       'close_source_branch' => false,
@@ -51,26 +29,18 @@ class PullRequestExample
         'commit' => {
           'hash' => 'bbbbbbbbbbbb',
           'links' => {
-            'self' => {
-              'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/commit/bbbbbbbbbbbb'
-            }
+            'self' => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/commit/bbbbbbbbbbbb' }
           }
         },
         'repository' => {
           'links' => {
-            'self' => {
-              'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket'
-            },
-            'avatar' => {
-              'href' => 'https://d3oaxc4q5k2d6q.cloudfront.net/m/dec446fd616d/img/language-avatars/default_16.png'
-            }
+            'self'   => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket' },
+            'avatar' => { 'href' => 'https://d3oaxc4q5k2d6q.cloudfront.net/m/dec446fd616d/img/language-avatars/default_16.png' }
           },
           'full_name' => 'jenkins-bitbucket/jenkins-bitbucket',
           'name' => 'jenkins-bitbucket'
         },
-        'branch' => {
-          'name' => 'master'
-        }
+        'branch' => { 'name' => 'master' }
       },
       'reason' => '',
       'closed_by' => nil,
@@ -78,26 +48,18 @@ class PullRequestExample
         'commit' => {
           'hash' => 'aaaaaaaaaaaa',
           'links' => {
-            'self' => {
-              'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/commit/aaaaaaaaaaaa'
-            }
+            'self' => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket/commit/aaaaaaaaaaaa' }
           }
         },
         'repository' => {
           'links' => {
-            'self' => {
-              'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket'
-            },
-            'avatar' => {
-              'href' => 'https://d3oaxc4q5k2d6q.cloudfront.net/m/dec446fd616d/img/language-avatars/default_16.png'
-            }
+            'self'   => { 'href' => 'https://api.bitbucket.org/2.0/repositories/jenkins-bitbucket/jenkins-bitbucket' },
+            'avatar' => { 'href' => 'https://d3oaxc4q5k2d6q.cloudfront.net/m/dec446fd616d/img/language-avatars/default_16.png' }
           },
           'full_name' => 'jenkins-bitbucket/jenkins-bitbucket',
           'name' => 'jenkins-bitbucket'
         },
-        'branch' => {
-          'name' => 'my-branch'
-        }
+        'branch' => { 'name' => 'my-branch' }
       },
       'state' => 'OPEN',
       'created_on' => '2014-07-29T00:59:26.973759+00:00',
