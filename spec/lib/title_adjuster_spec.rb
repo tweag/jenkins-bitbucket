ac3e33d1bf4f51c7b473ffcd2f74ae1ae7cf9f20
Mike Nicholaides <mike.nicholaides@gmail.com>
2014-09-04 22:16:56 -0400

    Rubocop

--- spec/lib/title_adjuster_spec.rb
+++ spec/lib/title_adjuster_spec.rb
@@ -6,42 +6,46 @@ describe TitleAdjuster do
   let(:title_adjuster) { described_class.new(good: 'GOOD', bad: 'BAD') }
 
   let(:message) do
-    double(StatusMessage, pull_request: double(PullRequest, title: title), ready_to_review?: ready_to_review?)
+    double(
+      StatusMessage,
+      ready_to_review?: ready_to_review?,
+      pull_request:     double(PullRequest, title: title)
+    )
   end
 
-  context "when the pull request is ready to review" do
+  context 'when the pull request is ready to review' do
     let(:ready_to_review?) { true }
 
-    context "when the title already contains the same status" do
+    context 'when the title already contains the same status' do
       let(:title) { 'GOOD pull request' }
       it { is_expected.to eq 'GOOD pull request' }
     end
 
-    context "when the title already contains the a different status" do
+    context 'when the title already contains the a different status' do
       let(:title) { 'BAD pull request' }
       it { is_expected.to eq 'GOOD pull request' }
     end
 
-    context "when the title does not contain a status" do
+    context 'when the title does not contain a status' do
       let(:title) { 'pull request' }
       it { is_expected.to eq 'GOOD pull request' }
     end
   end
 
-  context "when the pull request is not ready to review" do
+  context 'when the pull request is not ready to review' do
     let(:ready_to_review?) { false }
 
-    context "when the title already contains the same status" do
+    context 'when the title already contains the same status' do
       let(:title) { 'BAD pull request' }
       it { is_expected.to eq 'BAD pull request' }
     end
 
-    context "when the title already contains the a different status" do
+    context 'when the title already contains the a different status' do
       let(:title) { 'GOOD pull request' }
       it { is_expected.to eq 'BAD pull request' }
     end
 
-    context "when the title does not contain a status" do
+    context 'when the title does not contain a status' do
       let(:title) { 'pull request' }
       it { is_expected.to eq 'BAD pull request' }
     end
